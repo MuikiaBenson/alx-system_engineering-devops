b85b4a90dae415513abf28c74076a5e46d772f17
Muikia Benson <benmuigai20@gmail.com>
2024-04-19 23:55:40 +0300

    killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+# Puppet manifest to kill a process named killmenow
+exec { 'killmenow':
+ command => '/usr/bin/pkill killmenow',
+}
--- 0x0A-configuration_management/README.md
+++ 0x0A-configuration_management/README.md
@@ -1,2 +1,4 @@
 Configuration management
 0. create a file
+1. Install flask pip3
+2. Create a manifest named killmenow
